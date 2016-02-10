3649974adc3806a09c841902a6204e223da6294b
Vyas Giridharan <vyasgiridhar27@gmail.com>
2016-02-10 09:23:45 +0530

    Changed Audio url

--- app/src/main/java/hackerspace/invento/youtubedownloader/GetSong.java
+++ app/src/main/java/hackerspace/invento/youtubedownloader/GetSong.java
@@ -7,15 +7,12 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by invento on 29/5/15.
@@ -33,7 +30,7 @@ public class GetSong extends AsyncTask<Void, Void, Void> {
 
     void SetURL(String u, Context c) {
 
-        this.url = "http://YoutubeInMP3.com/fetch/?api=advanced&format=JSON&video="+u;
+        this.url = "http://www.youtubeinmp3.com/download/?video="+u+"&autostart=1";
         context = c;
 
     }
@@ -51,40 +48,7 @@ public class GetSong extends AsyncTask<Void, Void, Void> {
     @Override
     protected Void doInBackground(Void... arg0) {
 
-        String jsonStr = null;
-        BufferedReader reader = null;
-        try {
 
-            URL getUrl = new URL(this.url);
-            HttpURLConnection urlConnection = (HttpURLConnection) getUrl.openConnection();
-            urlConnection.setRequestProperty("User-Agent", USER_AGENT);
-            try {
-                 reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                jsonStr  = reader.readLine();
-
-            } finally {
-                if (reader != null) {
-                    reader.close();
-                }
-                urlConnection.disconnect();
-            }
-
-
-            Log.d("Response: ",jsonStr);
-
-
-
-                Document string = Jsoup.parse(jsonStr);
-                this.url = string.getElementById("Link").toString();
-                Log.d("THe URL = ",this.url);
-
-
-            return null;
-        }catch(Exception E){
-          /*  Toast.makeText(context,"Couldn't fetch Result",
-                    Toast.LENGTH_SHORT).show();*/
-        }
-        Log.d("THe URL = ",this.url);
 
         return null;
     }
